fc07b129146f765faa686ed9dd19fe5e5a113286
Caleb John <cajohn0205@gmail.com>
2021-06-16 16:01:38 -0700

    first commit

---
+++ src/rembg/bg.py
@@ -0,0 +1,172 @@
+import functools
+import io
+
+import numpy as np
+from PIL import Image
+from pymatting.alpha.estimate_alpha_cf import estimate_alpha_cf
+from pymatting.foreground.estimate_foreground_ml import estimate_foreground_ml
+from pymatting.util.util import stack_images
+from scipy.ndimage.morphology import binary_erosion
+from PIL import Image, ImageEnhance
+
+
+from .u2net import detect
+
+
+def alpha_matting_cutout(
+    img,
+    mask,
+    foreground_threshold,
+    background_threshold,
+    erode_structure_size,
+    base_size,
+):
+    size = img.size
+
+    img.thumbnail((base_size, base_size), Image.LANCZOS)
+    mask = mask.resize(img.size, Image.LANCZOS)
+
+    img = np.copy(np.asarray(img))
+    mask = np.copy(np.asarray(mask))
+
+    
+
+    super_threshold_indices = mask < 50
+    mask[super_threshold_indices] = 0
+
+    super_threshold_indices = mask > 240
+    mask[super_threshold_indices] = 255
+
+    mask_new = Image.fromarray(mask)
+    mask_new.save("mask.png")
+
+    # guess likely foreground/background
+    is_foreground = mask > 240
+    is_background = mask < 50
+
+    erode_structure_size = 3
+    # erode foreground/background
+    structure = None
+    if erode_structure_size > 0:
+        structure = np.ones((erode_structure_size, erode_structure_size), dtype=np.int)
+
+    is_foreground = binary_erosion(is_foreground, structure=structure)
+    is_background = binary_erosion(is_background, structure=structure, border_value=1)
+
+    # build trimap
+    # 0   = background
+    # 128 = unknown
+    # 255 = foreground
+    trimap = np.full(mask.shape, dtype=np.uint8, fill_value=128)
+
+    trimap[is_foreground] = 255
+    trimap[is_background] = 0
+
+    result_arr = np.dstack((img, trimap))
+    result = Image.fromarray(result_arr)
+    result.save("nomatting.png")
+
+
+    trimap = np.full(mask.shape, dtype=np.uint8, fill_value=200)
+    trimap[is_foreground] = 255
+    trimap[is_background] = 0
+
+    trimapImg = Image.fromarray(np.copy(trimap))
+    trimapImg.save("trimap.png")
+    # build the cutout image
+    img_normalized = img / 255.0
+    trimap_normalized = trimap / 255.0
+
+    alpha = estimate_alpha_cf(img_normalized, trimap_normalized)
+    foreground = estimate_foreground_ml(img_normalized, alpha, gradient_weight=0.01)
+    cutout = stack_images(foreground, alpha)
+
+    cutout = np.clip(cutout * 255, 0, 255).astype(np.uint8)
+
+    
+    
+    super_threshold_indices = cutout[:, :, 3] <= 10
+    cutout[:, :, 3][super_threshold_indices] = 0
+
+
+
+    super_threshold_indices = cutout[:, :, 3] > 10
+    cutout[:, :, 3][super_threshold_indices] = 255
+
+    cutout = Image.fromarray(cutout)
+    cutoutImg = Image.fromarray(np.copy(cutout))
+    cutoutImg.save("cutout.png")
+    cutout = cutout.resize(size, Image.LANCZOS)
+
+    return cutout
+
+
+def naive_cutout(img, mask):
+    empty = Image.new("RGBA", (img.size), 0)
+    cutout = Image.composite(img, empty, mask.resize(img.size, Image.LANCZOS))
+    return cutout
+
+
+@functools.lru_cache(maxsize=None)
+def get_model(model_name):
+    if model_name == "u2netp":
+        return detect.load_model(model_name="u2netp")
+    if model_name == "u2net_human_seg":
+        return detect.load_model(model_name="u2net_human_seg")
+    else:
+        return detect.load_model(model_name="u2net")
+
+
+def remove(
+    data,
+    model_name="u2net",
+    alpha_matting=True,
+    alpha_matting_foreground_threshold=240,
+    alpha_matting_background_threshold=40,
+    alpha_matting_erode_structure_size=1,
+    alpha_matting_base_size=1000,
+):
+    model = get_model(model_name)
+    img = Image.open(io.BytesIO(data)).convert("RGB")
+
+    enhancer = ImageEnhance.Sharpness(img)
+
+    factor = 2
+    imgs = enhancer.enhance(factor)
+    imgs.save('sharpened-image.png');
+
+    enhancer_2 = ImageEnhance.Contrast(imgs)
+
+
+    factor = 1.1 #increase contrast
+    im_c = enhancer_2.enhance(factor)
+    im_c.save('more-contrast-image.png')
+
+    factor = 1.1
+    enhancer_3 = ImageEnhance.Color(im_c)
+    im_color = enhancer_3.enhance(factor)
+    im_color.save('more-saturation-image.png')
+
+    factor = 0.9
+    enhancer_4 = ImageEnhance.Brightness(im_color)
+    im_output = enhancer_4.enhance(factor)
+    im_output.save('brightness-image.png')
+
+    mask = detect.predict(model, np.array(im_output)).convert("L")
+
+    if alpha_matting:
+        cutout = alpha_matting_cutout(
+            img,
+            mask,
+            alpha_matting_foreground_threshold,
+            alpha_matting_background_threshold,
+            alpha_matting_erode_structure_size,
+            alpha_matting_base_size,
+        )
+    else:
+        cutout = naive_cutout(img, mask)
+
+    bio = io.BytesIO()
+    cutout.save(bio, "PNG")
+
+    return bio.getbuffer()
---
+++ src/rembg/cmd/cli.py
@@ -0,0 +1,158 @@
+import argparse
+import glob
+import os
+from distutils.util import strtobool
+
+import filetype
+from tqdm import tqdm
+
+from ..bg import remove
+
+#from src.rembg.bg import remove
+
+def main():
+    model_path = '/home/caleb/Projects/PongoAI/rembg/src/rembg/saved_models'
+    
+    model_choices = [os.path.splitext(os.path.basename(x))[0] for x in set(glob.glob(model_path + "/*"))]
+    if len(model_choices) == 0:
+        model_choices = ["u2net", "u2netp", "u2net_human_seg"]
+
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument(
+        "-m",
+        "--model",
+        default="u2net",
+        type=str,
+        choices=model_choices,
+        help="The model name.",
+    )
+
+    ap.add_argument(
+        "-a",
+        "--alpha-matting",
+        nargs="?",
+        const=True,
+        default=False,
+        type=lambda x: bool(strtobool(x)),
+        help="When true use alpha matting cutout.",
+    )
+
+    ap.add_argument(
+        "-af",
+        "--alpha-matting-foreground-threshold",
+        default=240,
+        type=int,
+        help="The trimap foreground threshold.",
+    )
+
+    ap.add_argument(
+        "-ab",
+        "--alpha-matting-background-threshold",
+        default=10,
+        type=int,
+        help="The trimap background threshold.",
+    )
+
+    ap.add_argument(
+        "-ae",
+        "--alpha-matting-erode-size",
+        default=10,
+        type=int,
+        help="Size of element used for the erosion.",
+    )
+
+    ap.add_argument(
+        "-az",
+        "--alpha-matting-base-size",
+        default=1000,
+        type=int,
+        help="The image base size.",
+    )
+
+    ap.add_argument(
+        "-p",
+        "--path",
+        nargs=2,
+        help="An input folder and an output folder.",
+    )
+
+    ap.add_argument(
+        "-o",
+        "--output",
+        nargs="?",
+        default="-",
+        type=argparse.FileType("wb"),
+        help="Path to the output png image.",
+    )
+
+    ap.add_argument(
+        "input",
+        nargs="?",
+        default="-",
+        type=argparse.FileType("rb"),
+        help="Path to the input image.",
+    )
+
+    args = ap.parse_args()
+
+    r = lambda i: i.buffer.read() if hasattr(i, "buffer") else i.read()
+    w = lambda o, data: o.buffer.write(data) if hasattr(o, "buffer") else o.write(data)
+
+    if args.path:
+        full_paths = [os.path.abspath(path) for path in args.path]
+
+        input_paths = [full_paths[0]]
+        output_path = full_paths[1]
+
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        files = set()
+
+        for path in input_paths:
+            if os.path.isfile(path):
+                files.add(path)
+            else:
+                input_paths += set(glob.glob(path + "/*"))
+
+        for fi in tqdm(files):
+            fi_type = filetype.guess(fi)
+
+            if fi_type is None:
+                continue
+            elif fi_type.mime.find("image") < 0:
+                continue
+
+            with open(fi, "rb") as input:
+                with open(os.path.join(output_path, os.path.splitext(os.path.basename(fi))[0] + ".png"), "wb") as output:
+                    w(
+                        output,
+                        remove(
+                            r(input),
+                            model_name=args.model,
+                            alpha_matting=True,
+                            alpha_matting_foreground_threshold=args.alpha_matting_foreground_threshold,
+                            alpha_matting_background_threshold=args.alpha_matting_background_threshold,
+                            alpha_matting_erode_structure_size=args.alpha_matting_erode_size,
+                            alpha_matting_base_size=args.alpha_matting_base_size,
+                        ),
+                    )
+
+    else:
+        w(
+            args.output,
+            remove(
+                r(args.input),
+                model_name=args.model,
+                alpha_matting=True,
+                alpha_matting_foreground_threshold=args.alpha_matting_foreground_threshold,
+                alpha_matting_background_threshold=args.alpha_matting_background_threshold,
+                alpha_matting_erode_structure_size=args.alpha_matting_erode_size,
+                alpha_matting_base_size=args.alpha_matting_base_size,
+            ),
+        )
+
+
+if __name__ == "__main__":
+    main()
